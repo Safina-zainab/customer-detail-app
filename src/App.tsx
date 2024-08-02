69a3bfceca40bbb49daeadba45face77099584b2
safina zainab <safina.cumin@gmail.com>
2024-08-02 20:52:51 +0530

    Page setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import React, { useState } from 'react';
+import CustomerList from './components/CustomerList';
+import './styles.css';
 
-function App() {
+const App: React.FC = () => {
+  const [selectedCustomerId, setSelectedCustomerId] = useState<string | null>(null);
+
+  const handleSelectCustomer = (id: string) => {
+    setSelectedCustomerId(id);
+  };
 
   return (
-    <>
-    </>
-  )
-}
+    <div className="app">
+      <div className="sidebar">
+        <CustomerList onSelectCustomer={handleSelectCustomer} selectedCustomerId={selectedCustomerId} />
+      </div>
+      <div className="main-content">
+        {/* Details for the selected customer will be displayed here */}
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
